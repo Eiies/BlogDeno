11be1f2a8a7c25e7243c74be317d3a6411888af5
miku <da.weii@outlook.com>
2023-08-16 02:03:44 +0800

    波浪

--- src/islands/Wave.tsx
+++ src/islands/Wave.tsx
@@ -2,22 +2,16 @@ import { useEffect, useRef } from "https://esm.sh/preact@10.15.1/hooks";
 
 export default function Wave() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
   const waveLayers = [
     {
-      // 振幅
-      amplitude: 50 + Math.random() * 20, // 添加随机性
-      // 频率
+      amplitude: 50 + Math.random() * 20,
       frequency: 0.01,
-      // 速度
       speed: 0.1,
-      // Y 轴偏移
       yOffset: 300,
-      // 颜色
       color: "rgba(36, 135, 218, 0.3)",
     },
     {
-      amplitude: 60 + Math.random() * 20, // 添加随机性
+      amplitude: 60 + Math.random() * 20,
       frequency: 0.005,
       speed: 0.15,
       yOffset: 250,
@@ -25,6 +19,7 @@ export default function Wave() {
     },
   ];
   let time = 0;
+  let animationFrameId: number | null = null;
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -39,14 +34,10 @@ export default function Wave() {
       return;
     }
 
-    ctxRef.current = ctx;
-
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
     const animate = () => {
-      requestAnimationFrame(animate);
-
       if (!ctx) {
         return;
       }
@@ -71,15 +62,28 @@ export default function Wave() {
         ctx.fill();
         ctx.closePath();
       }
+
+      animationFrameId = requestAnimationFrame(animate);
     };
 
-    animate();
+    animationFrameId = requestAnimationFrame(animate);
+
+    return () => {
+      if (animationFrameId) {
+        cancelAnimationFrame(animationFrameId);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      if (animationFrameId) {
+        cancelAnimationFrame(animationFrameId);
+      }
+    };
   }, []);
 
   return (
-    <>
-      <canvas ref={canvasRef} id="waveCanvas" className="h-96 w-full">
-      </canvas>
-    </>
+    <canvas ref={canvasRef} id="waveCanvas" className="h-96 w-full"></canvas>
   );
 }
